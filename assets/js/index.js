7e868b953e8a02aaaf3c230fe16ac9eaa681c2d7
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-11 18:24:42 +0300

    Final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 "use strict";
 
 class User {
+  /**
+   * 
+   * @param {string} name 
+   * @param {string} surname 
+   */
   constructor(name, surname) {
     this.name = name;
     this.surname = surname;
@@ -8,7 +13,7 @@ class User {
 
   set name(newName) {
     if (typeof newName !== "string") {
-      throw new TypeError("ERROR!!! Enter not a name");
+      throw new TypeError(`ERROR!!! Parameter "${newName}" has unappropriate type`);
     }
     this._name = newName;
   }
@@ -19,7 +24,7 @@ class User {
 
   set surname(newSurname) {
     if (typeof newSurname !== "string") {
-      throw new TypeError("ERROR!!! Enter not a surname");
+      throw new TypeError(`ERROR!!! Parameter "${newSurname}" has unappropriate type`);
     }
     this._surname = newSurname;
   }
@@ -34,18 +39,35 @@ class User {
 }
 
 class Student extends User {
+  /**
+   * 
+   * @param {string} name 
+   * @param {string} surname 
+   * @param {number} year 
+   */
   constructor(name, surname, year) {
     super(name, surname);
     this.year = year;
   }
 
+  set year(newYear) {
+    if (typeof newYear !== "number") {
+      throw new TypeError(`ERROR!!! Parameter "${newYear}" has unappropriate type`);
+    }
+    this._year = newYear;
+  }
+
+  get year() {
+    return this._year;
+  }
+
   get course() {
     const course = new Date().getFullYear() - this.year;
     if (course < 1 || course > 5) {
-      throw new RangeError("ERROR!!! No such course found");
+      throw new RangeError(`ERROR!!! No such "${course}" course found`);
     }
     return `student ${this.fullName} at ${course} course university`;
   }
 }
 
-const student = new Student("Bob", "Smith", 2018);
+const student = new Student("Bob", "Smith", 2019);
